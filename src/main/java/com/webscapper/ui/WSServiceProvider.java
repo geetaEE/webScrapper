2336b3f9334f2b78ccdd103f6924efcb627f5a2c
vivek.yadav@impetus.co.in <vivek.yadav@impetus-N437.impetus.co.in>
2014-09-11 18:55:00 +0530

    WSServicePovider_1.0

--- src/main/java/com/webscapper/ui/WSServiceProvider.java
+++ src/main/java/com/webscapper/ui/WSServiceProvider.java
@@ -1,6 +1,33 @@
 package com.webscapper.ui;
 
+import com.webscapper.factory.ExtractServiceFactory;
+import com.webscapper.request.ExtractRequest;
+import com.webscapper.response.ExtractResponse;
+import com.webscrapper.constants.ContentType;
+
 public class WSServiceProvider 
 {
-
+	public ExtractRequest buildExtractRequest(String url, ContentType contentType)
+	{
+		ExtractRequest extractRequest = new ExtractRequest();
+		extractRequest.setUrl(url);
+		extractRequest.setContentType(contentType);
+		return extractRequest;
+	}
+	
+	public ExtractResponse startExtraction(ExtractRequest extractRequest)
+	{
+		ExtractResponse extractResponse = ExtractServiceFactory.getInstance(extractRequest.getContentType()).extract(extractRequest);
+		return extractResponse;
+	}
+	
+	public String[] fetchColumnNameForPreview(ExtractResponse extractResponse)
+	{
+		return null;
+	}
+	
+	public String[][] fetchColumnValuesForPreview(ExtractResponse extractResponse)
+	{
+		return null;
+	}
 }

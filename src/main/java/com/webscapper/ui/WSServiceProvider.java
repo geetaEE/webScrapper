c797909b211a79e5a996a81aebf90403e8d7ec10
vivek.yadav@impetus.co.in <vivek.yadav@impetus-N437.impetus.co.in>
2014-09-12 10:15:56 +0530

    WSServiceProvider_12_09_Ver1.0

--- src/main/java/com/webscapper/ui/WSServiceProvider.java
+++ src/main/java/com/webscapper/ui/WSServiceProvider.java
@@ -1,9 +1,15 @@
 package com.webscapper.ui;
 
+import java.util.List;
+
+import com.webscapper.factory.ExportServiceFactory;
 import com.webscapper.factory.ExtractServiceFactory;
+import com.webscapper.request.ExportRequest;
 import com.webscapper.request.ExtractRequest;
+import com.webscapper.response.ExportResponse;
 import com.webscapper.response.ExtractResponse;
 import com.webscrapper.constants.ContentType;
+import com.webscrapper.constants.ExportType;
 
 public class WSServiceProvider 
 {
@@ -15,15 +21,38 @@ public class WSServiceProvider
 		return extractRequest;
 	}
 	
-	public ExtractResponse startExtraction(ExtractRequest extractRequest)
+	public ExportRequest buildExportRequest(String url, String title, ExtractResponse extractResponse, ExportType exportType, List<String> tagsList, String location)
+	{
+		ExportRequest exportRequest = new ExportRequest();
+		exportRequest.setUrl(url);
+		exportRequest.setTitle(title);
+		exportRequest.setExtractResponse(extractResponse);
+		exportRequest.setExportType(exportType);
+		exportRequest.setTagsList(tagsList);
+		exportRequest.setLocation(location);
+		
+		return exportRequest;
+	}
+	
+	public ExtractResponse executeExtractOperation(ExtractRequest extractRequest)
 	{
 		ExtractResponse extractResponse = ExtractServiceFactory.getInstance(extractRequest.getContentType()).extract(extractRequest);
 		return extractResponse;
 	}
 	
+	public ExportResponse executeExportOperation(ExportRequest exportRequest)
+	{
+		ExportResponse exportResponse = ExportServiceFactory.getInstance(exportRequest.getExportType()).export(exportRequest);
+		return exportResponse;
+	}
+	
 	public String[] fetchColumnNameForPreview(ExtractResponse extractResponse)
 	{
-		return null;
+		List<List<List<String>>> tables = extractResponse.getTables();
+        List<String> columns = tables.get(0).get(0);
+        String[] columnNames = columns.toArray(new String[columns.size()]);
+        return columnNames;
+        
 	}
 	
 	public String[][] fetchColumnValuesForPreview(ExtractResponse extractResponse)

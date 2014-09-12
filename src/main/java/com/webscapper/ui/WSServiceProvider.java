b01e78121bb1a3cf7cfc98f6debab3621c0d552c
vivek.yadav@impetus.co.in <vivek.yadav@impetus-N437.impetus.co.in>
2014-09-12 18:08:41 +0530

    ServiceProviderUpdate

--- src/main/java/com/webscapper/ui/WSServiceProvider.java
+++ src/main/java/com/webscapper/ui/WSServiceProvider.java
@@ -1,6 +1,9 @@
 package com.webscapper.ui;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.webscapper.factory.ExportServiceFactory;
 import com.webscapper.factory.ExtractServiceFactory;
@@ -48,11 +51,57 @@ public class WSServiceProvider
 	
 	public String[][] fetchTabularPreviewData(ExtractResponse extractResponse)
 	{
-		return null;
+		String[][] columnArray = new String[4][4];
+		List<List<List<String>>> tablesList = extractResponse != null ? extractResponse.getTables() : null;
+        if (tablesList != null && tablesList.size() > 0) 
+        {
+        	List<List<String>> table = tablesList.get(0);                            
+        	int rowCounter = 0;           
+            for (List<String> row : table) 
+            {              
+            	columnArray[rowCounter] = new String[4];
+            	int columnCounter = 0;
+            	for(String columnValue : row)
+                {
+                	if(columnCounter == 4)
+                	{
+                		break;
+                	}
+                	columnArray[rowCounter][columnCounter]=columnValue;
+            		columnCounter++;
+                }
+            	
+            	if(columnCounter < 4)
+            	{
+            		for(int i = columnCounter ; i<= 3 ; i++)
+            				columnArray[rowCounter][i]="";
+            	}
+            	
+            	rowCounter++;
+            }
+            if(rowCounter < 4 )
+            {            	
+            	for(int i = rowCounter ; i<= 3 ; i++)
+            	{
+            		columnArray[rowCounter] = new String[4];
+            		for(int j = 0 ; j<= 3 ; j++)
+            		{
+            			columnArray[rowCounter][j]="";
+            		}
+            	}
+            }
+        }
+        
+        return columnArray;
 	}
 	
 	public String[] fetchColumnNameForPreview(ExtractResponse extractResponse)
-	{
-		return null;
+	{		
+        String[] columnArray = new String[4];
+        columnArray[0] = "Column_1";
+        columnArray[1] = "Column_2";
+        columnArray[2] = "Column_3";
+        columnArray[3] = "Column_4";
+        return columnArray;
 	}
 }

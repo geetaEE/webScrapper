fd159ed2f0cff6f95812dd20e39e6a8bace91daf
Anupam Jain <anupam.jain@impetus.co.in>
2014-09-15 11:52:40 +0530

    Initial test db class

--- src/test/java/com/webscapper/service/TestExportService.java
+++ src/test/java/com/webscapper/service/TestExportService.java
@@ -19,34 +19,34 @@ public class TestExportService {
         List<String> columnValue1 = new ArrayList<String>();
         columnValue1.add("Test1");
         columnValue1.add("Test2");
-        
+
         List<String> columnValue2 = new ArrayList<String>();
         columnValue2.add("Test4");
         columnValue2.add("Test5");
-        
+
         List<List<String>> rowList1 = new ArrayList<List<String>>();
         rowList1.add(columnValue1);
         rowList1.add(columnValue2);
-        
+
         List<List<String>> rowList2 = new ArrayList<List<String>>();
         rowList2.add(columnValue2);
-        
-        List<List<List<String>>> tableList = new ArrayList<List<List<String>>>();       
+
+        List<List<List<String>>> tableList = new ArrayList<List<List<String>>>();
         tableList.add(rowList1);
         tableList.add(rowList2);
-        
+
         ExportRequest exportRequest = new ExportRequest();
         exportRequest.setTitle("Test");
         exportRequest.setUrl("https://www.httpsnow.org/");
         exportRequest.setExportType(ExportType.DB);
-        
+
         ExtractResponse extractResponse = new ExtractResponse();
         extractResponse.setTables(tableList);
         exportRequest.setExtractResponse(extractResponse);
-        
+
         ExportService exportService = ExportServiceFactory.getInstance(exportRequest.getExportType());
         exportService.export(exportRequest);
-        
-        //Needs to put assert
+
+        // Needs to put assert
     }
 }

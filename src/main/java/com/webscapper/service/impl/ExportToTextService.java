bcb1d6bd132a9b886fbd782f3367a31d8cdd6e41
geetaEE <chaudhary1586@gmail.com>
2014-09-16 18:29:25 +0530

    Export to text Impl

--- src/main/java/com/webscapper/service/impl/ExportToTextService.java
+++ src/main/java/com/webscapper/service/impl/ExportToTextService.java
@@ -8,8 +8,40 @@ public class ExportToTextService  implements ExportService{
 
 	@Override
 	public ExportResponse export(ExportRequest request) {
-		// TODO Auto-generated method stub
-		return null;
+		String fileName = request.getLocation() + File.separator + request.getTitle()
+				+ ".txt";
+		ExportResponse exportResponse = new ExportResponse();
+		try
+			{
+			FileWriter writer = new FileWriter(fileName);
+			ExtractResponse response = request.getExtractResponse();
+			List<String> tagsList = request.getTagsList();
+			Map<TagType, String> tagData = response != null ? response
+					.getTagDataMap() : null;
+	
+			if (tagsList != null) 
+			{
+				for (String tags : tagsList)
+				{
+					for (Map.Entry<TagType, String> entry : tagData.entrySet()) 
+					{
+						if (entry.getKey().getDisplayName().equals(tags)) 
+						{
+							writer.append(entry.getKey() + "\n");
+							writer.append(entry.getValue() + "\n");
+							writer.append("\n");
+						}
+					}
+				}
+				exportResponse.setSuccess(true);
+			}
+			writer.close();
+
+			
+		} catch (Exception e) {
+			exportResponse.setSuccess(false);
+		}
+		return exportResponse;
 	}
 
 }

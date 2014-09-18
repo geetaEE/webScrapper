e47b5eaeeb5955646238e887345ebc697b3b7a4b
vivek.yadav@impetus.co.in <vivek.yadav@impetus-N437.impetus.co.in>
2014-09-18 10:14:23 +0530

    URLUtilTest Commit

--- src/test/java/com/webscrapper/ui/URLUtilTest.java
+++ src/test/java/com/webscrapper/ui/URLUtilTest.java
@@ -1,8 +1,9 @@
 package com.webscrapper.ui;
 
-import org.junit.Test;
+import org.junit.*;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import com.webscapper.ui.URLUtil;
 
 @RunWith(JUnit4.class)
 public class URLUtilTest 
@@ -10,12 +11,28 @@ public class URLUtilTest
 	@Test
     public void testIsValidURL() 
 	{
+		//Scenario 1 : Positive Scenario.
+		String url = "http://www.google.com";
+		Boolean result = URLUtil.isValidURL(url);
+		Assert.assertTrue("URL validaton result should be true.",result);
 		
+		//Scenario 2 : Negative Scenario.
+		url = "http://www.vivekyadav.com";
+		result = URLUtil.isValidURL(url);
+		Assert.assertFalse("URL validaton result should be false.",result);
     }
 	
 	@Test
     public void testIsValidURLForConnection() 
 	{
+		//Scenario 1 : Positive Scenario.
+		String url = "http://www.google.com";
+		Boolean result = URLUtil.isValidURLForConnection(url);
+		Assert.assertTrue("URL connection validaton result should be true.",result);
 		
+		//Scenario 2 : Negative Scenario.
+		url = "http://www.vivekyadav.com";
+		result = URLUtil.isValidURLForConnection(url);
+		Assert.assertFalse("URL connection validaton result should be false.",result);
     }
 }

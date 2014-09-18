7907ac93cfad98faf3cd400fd73dc4326e29f359
vivek.yadav@impetus.co.in <vivek.yadav@impetus-N437.impetus.co.in>
2014-09-18 11:26:57 +0530

    TestWebScrapperUtil Fix

--- src/test/java/com/webscrapper/ui/TestWebScrapperUtil.java
+++ src/test/java/com/webscrapper/ui/TestWebScrapperUtil.java
@@ -1,33 +1,82 @@
 package com.webscrapper.ui;
 
+import java.awt.Point;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.swing.JList;
+
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+import com.webscapper.ui.CheckListItem;
+import com.webscapper.ui.WebScrapperUtil;
+
 @RunWith(JUnit4.class)
 public class TestWebScrapperUtil 
 {
-	@Test
+	@Test 
     public void testGetSelectedListItems() 
 	{
+		JList imageList = new JList(new CheckListItem[]{});
 		
+		List<String> imagesList = Arrays.asList(new String[]{"Test1","Test2"});
+				
+		imageList.setListData(WebScrapperUtil.getCheckListItemArray(imagesList));
+		
+		//Scenario 1: When none of item is selecated.
+		List<CheckListItem> selectedItemList = WebScrapperUtil.getSelectedListItems(imageList);
+		
+		Assert.assertEquals("Selected Item length should be zero.", 0, selectedItemList.size());
+		
+		//Scenario 2: When single item is selected.
+		int index = imageList.locationToIndex(new Point(0,0));
+        CheckListItem item = (CheckListItem)imageList.getModel().getElementAt(index);
+        
+        // Toggle selected state
+        
+        item.setSelected(! item.isSelected());
+        
+        // Repaint cell
+        
+        imageList.repaint(imageList.getCellBounds(index, index));
+		
+		selectedItemList = WebScrapperUtil.getSelectedListItems(imageList);
+		Assert.assertEquals("Selected Item length should be one.", 1, selectedItemList.size());
     }
 	
-	@Test
+    @Test 
     public void testGetSelectedListItemValues() 
 	{
+		JList imageList = new JList(new CheckListItem[]{});
 		
+		List<String> imagesList = Arrays.asList(new String[]{"Test1","Test2"});
+				
+		imageList.setListData(WebScrapperUtil.getCheckListItemArray(imagesList));
+		
+		//Scenario 1: When none of item is selecated.
+		List<String> selectedValues= WebScrapperUtil.getSelectedListItemValues(imageList);
+		Assert.assertEquals("Selected Item length should be zero.", 0, selectedValues.size());
+		
+		//Scenario 2: When single item is selected.
+		int index = imageList.locationToIndex(new Point(0,0));
+        CheckListItem item = (CheckListItem)imageList.getModel().getElementAt(index);        
+        item.setSelected(! item.isSelected());
+        imageList.repaint(imageList.getCellBounds(index, index));
+        
+        selectedValues= WebScrapperUtil.getSelectedListItemValues(imageList);
+		Assert.assertEquals("Selected Item length should be one.", 1, selectedValues.size());
     }
 	
-	@Test
-    public void testGetSelectedListValues() 
-	{
-		
-    }
-	
-	@Test
+    @Test    
     public void testGetCheckListItemArray() 
 	{
-		
+    	List<String> imagesList = Arrays.asList(new String[]{"Test1","Test2"});
+    	
+    	CheckListItem[] checkListItems = WebScrapperUtil.getCheckListItemArray(imagesList);
+    	
+    	Assert.assertEquals("Item count should be two.", 2, checkListItems.length);
     }	
 }

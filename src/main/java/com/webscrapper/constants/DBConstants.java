e594d1f43bde0c4672f619c16463065e22f4b6c8
ruby.jha <ruby.jha@Impetus-N392.impetus.co.in>
2014-09-19 16:10:40 +0530

    test

--- src/main/java/com/webscrapper/constants/DBConstants.java
+++ src/main/java/com/webscrapper/constants/DBConstants.java
@@ -6,7 +6,7 @@ package com.webscrapper.constants;
  */
 public class DBConstants {
 	public static final String DB_NAME ="EE_J5_DB";
-	public static final String MONGO_SERVER = "127.0.0.1";
+	public static final String MONGO_SERVER = "192.168.206.210";
 	public static final int PORT_NO = 27017;
 	public static final String USER_NAME ="admin";
 	public static final String PASSWORD ="welcome";

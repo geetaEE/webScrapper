9a74d60373dd36ed7851068f42f66f10eba28a2a
ruby.jha <ruby.jha@Impetus-N392.impetus.co.in>
2014-09-19 15:25:44 +0530

    Junit test case fix

--- src/test/java/com/webscapper/service/TestExportService.java
+++ src/test/java/com/webscapper/service/TestExportService.java
@@ -1,11 +1,15 @@
 package com.webscapper.service;
 
+import static org.junit.Assert.assertFalse;
+
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import com.webscapper.factory.ExportServiceFactory;
@@ -20,8 +24,28 @@ import com.webscrapper.service.ExportService;
  * @author ruby.jha
  * Test export service.
  */
-public class TestExportService {
-    /** Test DB export. */
+public class TestExportService { 
+	
+	/** The exportRequest. */
+	private ExportRequest exportRequest;	
+	/** The extractResponse. */
+	private ExtractResponse extractResponse;
+	
+	@Before
+	public void setUp() {
+		exportRequest = new ExportRequest();		
+		extractResponse = new ExtractResponse();
+		exportRequest.setTitle("Test");
+		exportRequest.setLocation(System.getProperty("user.dir"));
+		exportRequest.setExportType(ExportType.DOC);
+		exportRequest.setExtractResponse(extractResponse);
+	}
+	
+	/**
+	   * Method under test: export()
+	   * Scenario: Verifying the behavior when table List is not null.    
+	   * Expectation: Data should be saved to DB successfully.
+	   */
     @Test
 	public void testExportToDB() {
 		List<String> columnValue1 = new ArrayList<String>();
@@ -42,13 +66,10 @@ public class TestExportService {
 		List<List<List<String>>> tableList = new ArrayList<List<List<String>>>();
 		tableList.add(rowList1);
 		tableList.add(rowList2);
-
-		ExportRequest exportRequest = new ExportRequest();
 		exportRequest.setTitle("Test");
 		exportRequest.setUrl("https://www.httpsnow.org/");
 		exportRequest.setExportType(ExportType.DB);
 
-		ExtractResponse extractResponse = new ExtractResponse();
 		extractResponse.setTables(tableList);
 		exportRequest.setExtractResponse(extractResponse);
 
@@ -56,33 +77,57 @@ public class TestExportService {
 				.getInstance(exportRequest.getExportType());
 		ExportResponse exportResponse = exportService.export(exportRequest);
 		Assert.assertTrue(exportResponse.isSuccess() == true);
+		Assert.assertNotNull(tableList);
+		Assert.assertNotNull(exportRequest.getUrl());
 	}
     
+    /**
+     * Method under test: export()
+     * Scenario: Verifying the behavior when Tag Data Map and tags list are not null.    
+     * Expectation: Data should be exported to doc successfully.
+     */
     @Test
-	public void testExportToDoc() {
-		ExtractResponse response = new ExtractResponse();
+	public void testExportToDoc() {		
 		Map<TagType, String> tagDataMap = new LinkedHashMap<TagType, String>();
 		tagDataMap.put(TagType.DIV, "Div Text");
 		tagDataMap.put(TagType.SPAN, "Span Text");
 		tagDataMap.put(TagType.ANCHOR, "Anchor Text");
 		tagDataMap.put(TagType.BOLD, "Bold");
 		tagDataMap.put(TagType.PARAGRAPH, "Paragraph Text");
-		response.setTagDataMap(tagDataMap);
+		extractResponse.setTagDataMap(tagDataMap);
 		List<String> tagsList = new ArrayList<String>();
 		tagsList.add("Div");
 		tagsList.add("Span");
 		tagsList.add("Hyper Link");
 		tagsList.add("Bold Text");
-		tagsList.add("Paragraph");
-		ExportRequest exportRequest = new ExportRequest();
-		exportRequest.setTitle("Test");
-		exportRequest.setLocation("D:\\EE");
-		exportRequest.setExportType(ExportType.DOC);
-		exportRequest.setExtractResponse(response);
+		tagsList.add("Paragraph");			
 		exportRequest.setTagsList(tagsList);
 		ExportService exportService = ExportServiceFactory
 				.getInstance(exportRequest.getExportType());
 		ExportResponse exportResponse = exportService.export(exportRequest);
 		Assert.assertTrue(exportResponse.isSuccess() == true);
+		Assert.assertNotNull(tagDataMap);
+		Assert.assertNotNull(tagsList);
 	}
+    
+    /**
+     * Method under test: export()
+     * Scenario: Verifying the behavior when tag list is Null.    
+     * Expectation: Data will not be saved & method will not throw any Exception.
+     */
+    @Test
+    public void testExportToDocWhenTagsListIsNull() 
+    {      
+	  exportRequest.setTagsList(null);
+      ExportService exportService = ExportServiceFactory.getInstance(exportRequest.getExportType());
+      exportService.export(exportRequest);
+      assertFalse(exportService.export(exportRequest).isSuccess());
+    }
+    
+    @After
+    public void destroy()
+    {
+    	exportRequest = null;    	
+    	extractResponse = null;		
+    }
 }

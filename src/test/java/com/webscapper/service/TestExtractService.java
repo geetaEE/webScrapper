944d34201e2aac1e9d4a42be0c224771d50163e2
Anupam Jain <anupam.jain@impetus.co.in>
2014-09-19 17:01:32 +0530

    Fixed test case

--- src/test/java/com/webscapper/service/TestExtractService.java
+++ src/test/java/com/webscapper/service/TestExtractService.java
@@ -68,7 +68,7 @@ public class TestExtractService {
         ExtractResponse response = ExtractServiceFactory.getInstance(request.getContentType()).extract(request);
         List<List<List<String>>> tables = response.getTables();
         List<String> columns = tables.get(0).get(0);
-        Assert.assertEquals(6, tables.size());
+        Assert.assertEquals(7, tables.size());
         Assert.assertEquals(5, tables.get(0).size());
         Assert.assertEquals(4, columns.size());
         Assert.assertEquals("Number", columns.get(0));

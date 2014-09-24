535399e007b51dee618ab15028123d49e5ea6079
vivek.yadav@impetus.co.in <vivek.yadav@impetus-N437.impetus.co.in>
2014-09-24 17:07:47 +0530

    Sonar Issue Fix

--- src/main/java/com/webscrapper/constants/UIConstants.java
+++ src/main/java/com/webscrapper/constants/UIConstants.java
@@ -5,4 +5,5 @@ public interface UIConstants
 	public static String TYPE = "Type";
 	public static String FOUND_PLACES = "No. Of Found Places";
 	public static String BLANK = "";
+	public static String WEB_SCRAPPER = "Web Scrapper";
 }

4de4b1560a86dddda76e83207b9d9a316fbedf5a
vivek.yadav@impetus.co.in <vivek.yadav@impetus-N437.impetus.co.in>
2014-09-25 18:47:50 +0530

    Assert Fix

--- src/test/java/com/webscrapper/connection/TestMongoConnectionManager.java
+++ src/test/java/com/webscrapper/connection/TestMongoConnectionManager.java
@@ -1,8 +1,7 @@
 package com.webscrapper.connection;
 
-import junit.framework.Assert;
-
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
